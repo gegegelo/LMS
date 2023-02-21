87688bf576198020ac4a482340966f8e049cc5a2
JayeXushi <121046458+JayeXushi@users.noreply.github.com>
2023-02-21 16:58:51 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>DepEd Management System</title>
+        <?php require 'links.php'; ?>
+
+        <?php include 'script.php'; ?>
+    </head>
+    <body>  
+        <div class="wrapper" id="wrapper">
+        <!-- Hero Images, Navigation, Texts -->
+            <section class="top">
+                <?php include 'index-nav.php'; ?>
+                <?php include 'carousel.php'; ?>
+            </section>
+        <!-- Footer -->
+        <?php include 'footer.php'; ?>
+        </div>
+    </body>
+</html>

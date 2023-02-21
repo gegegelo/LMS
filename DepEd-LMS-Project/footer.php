04d7263f7bc787941f9ee56ef75255572ed942aa
JayeXushi <121046458+JayeXushi@users.noreply.github.com>
2023-02-21 17:00:48 +0800

    Update footer.php

--- DepEd-LMS-Project/footer.php
+++ DepEd-LMS-Project/footer.php
@@ -31,7 +31,7 @@
 
     <div class="col-md-5 offset-md-1 mb-3">
       <form action="php/subscribe.php" method="POST">
-        <h5 class="text-white">Subscribe to our newsletter</h5>
+        <h5>Subscribe to our newsletter</h5>
         <p class="text-muted">Monthly digest of what's new and exciting from us.</p>
         <div class="d-flex flex-column flex-sm-row w-100 gap-2">
           <label for="newsletter1" class="visually-hidden">Email address</label>

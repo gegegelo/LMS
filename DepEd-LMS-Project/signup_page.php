04dccf0e643a8d3c4e60569a90cf7f81afca56c1
Mavkun <delacruzmarkjoseph28@gmail.com>
2023-02-22 18:05:00 +0800

    Sign up style

--- DepEd-LMS-Project/signup_page.php
+++ DepEd-LMS-Project/signup_page.php
@@ -5,8 +5,11 @@
 			DepEd E-Learning Management
 		</title>
 		<style>
-			input {
-    			border: 1.5px solid #030337;
+			body{
+				background-color: lightgray;
+			}
+			input, select{
+    			border: 3px solid lightgreen;
     			border-radius: 4px;
     			padding: 7px 30px;
 			}
@@ -17,6 +20,27 @@
     			padding: 7px 45px;
     			margin: 0px 135px
 			}
+			#form-div{
+				background-color: #030337;
+			}
+			#signup-title{
+				background: #0E86C7;
+				padding: 10px;
+				color: white;
+			}
+			.flabel{
+				font-size: 25px;
+				font-weight: bold;
+
+			}
+			#form-background{
+				background-color: white;
+				width: 70%;
+				margin-right: auto;
+				margin-left: auto;
+			}
+
+
 		</style>
 		<meta charset="UTF-8">
     	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -25,48 +49,59 @@
 	</head>
 	<body>
     <?php include 'index-nav.php'; ?>
-		<form class="center_form" action="new_user_form_handler.php" method="POST" id="new_user_from">
-			<h2><i class="fa fa-user-plus" aria-hidden="true"></i> CREATE NEW USER ACCOUNT</h2>
+		<center>
+			<div id="form-div">
+			<div id="form-background"><form class="center_form" action="new_user_form_handler.php" method="POST" id="new_user_from">
+			<h2 id="signup-title"><i class="fa fa-user-plus" aria-hidden="true"></i> CREATE NEW USER ACCOUNT</h2>
 			<br>
+			
 			<table cellpadding='10'>
 				<strong>ENTER LOGIN DETAILS</strong>
 				<tr>
-					<td>Enter a username  </td>
+					<td class="flabel">Enter Username  </td>
 					<td><input type="text" name="username" required><br><br></td>
 				</tr>
 				<tr>
-					<td>Enter your  password  </td>
+					<td class="flabel">Enter Password  </td>
 					<td><input type="password" name="password" required><br><br></td>
 				</tr>
                 <tr>
-					<td>Enter confirmed  password  </td>
+					<td class="flabel">Confirm  Password  </td>
 					<td><input type="password" name="confirmPassword" required><br><br></td>
 				</tr>
 				<tr>
-					<td>Enter your Email ID</td>
+					<td class="flabel">Enter Email </td>
 					<td><input type="text" name="email" required><br><br></td>
+				</tr>
+				<tr>
+					<td class="flabel">Select Category</td>
+					<td><select>
+						<option>Student</option>
+						<option>Teacher</option>
+					</select><br><br></td>
 				</tr>
 			</table>
 			<br>
 			<table cellpadding='10'>
 				<strong>PERSONAL DETAILS</strong>
 				<tr>
-					<td>Enter your first name  </td>
-					<td><input type="text" name="name" required><br><br></td>
+					<td class="flabel">Enter Firstname  </td>
+					<td ><input type="text" name="name" required><br><br></td>
 				</tr>
                 <tr>
-					<td>Enter your last name  </td>
+					<td class="flabel">Enter Lastname  </td>
 					<td><input type="text" name="name" required><br><br></td>
 				</tr>
 				<tr>
-					<td>Enter your phone number</td>
+					<td class="flabel">Enter Phone Number</td>
 					<td><input type="text" name="phone_no" required><br><br></td>
 				</tr>
 			</table>
+			
 			<br>
 			<input type="submit" value="Submit" name="Submit">
 			<br>
-		</form>
+		</form></div></div></center>
 		<?php include 'script.php'; ?>
 	</body>
 </html>

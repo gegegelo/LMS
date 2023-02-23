99266f67cc4b8b9f29dc0e9c1712ee12ce22b768
Angelo A. Ramos <66189567+gegegelo@users.noreply.github.com>
2023-02-23 08:31:48 +0800

    Update carousel.php

--- DepEd-LMS-Project/carousel.php
+++ DepEd-LMS-Project/carousel.php
@@ -10,41 +10,67 @@
 
     <div class="carousel-item active">
       <img src="img/coverr.jpg" class="d-block" alt="...">
-      <div class="hero__text">
-        <h1 class="text-sm-center font-700 font-shadow text-white">MATATAG</h1>
-        <p class="text-sm-center font-shadow text-white">Department of Education</p>
-        <a href="Things-to-do.php"><button type="button" class="mt-4 font-700">About</button></a>
-      </div>
       <div class="background"></div>
     </div>
 
     <div class="carousel-item">
       <img src="img/sara.png" class="d-block" alt="...">
-      <div class="hero__text">
-        <h1 class="text-sm-center font-700 font-shadow text-white"></h1>
-        <p class="text-sm-center font-shadow text-white"></p>
-        <a href="Things-to-do.php"><button type="button" class="mt-4 font-700">About</button></a>
-      </div>
       <div class="background"></div>
     </div>
 
     <div class="carousel-item">
       <img src="img/vp-brio.jpg" class="d-block" alt="...">
-      <div class="hero__text">
-        <h1 class="text-sm-center font-700 font-shadow text-white"></h1>
-        <p class="text-sm-center font-shadow text-white"></p>
-      </div>
       <div class="background"></div>
 
     </div>
 
     <div class="carousel-item">
-      <img src="img/jci-1.png" class="d-block" alt="...">
+      <img src="img/jci-poweredby.png" class="d-block" alt="...">
       <div class="hero__text">
-        <h1 class="text-sm-center font-700 font-shadow text-white">JCI Makati</h1>
-        <p class="text-sm-center font-shadow text-white"></p>
-        <a href="Things-to-do.php">
-          <button type="button" class="mt-4 font-700">About</button>
+        </a>
+      </div>
+      <div class="background"></div>
+    </div>
+
+  </div>
+
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div><!-- Carousel -->
+<div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+  </div>
+  <div class="carousel-inner">
+
+    <div class="carousel-item active">
+      <img src="img/coverr.jpg" class="d-block" alt="...">
+      <div class="background"></div>
+    </div>
+
+    <div class="carousel-item">
+      <img src="img/sara.png" class="d-block" alt="...">
+      <div class="background"></div>
+    </div>
+
+    <div class="carousel-item">
+      <img src="img/vp-brio.jpg" class="d-block" alt="...">
+      <div class="background"></div>
+
+    </div>
+
+    <div class="carousel-item">
+      <img src="img/jci-poweredby.png" class="d-block" alt="...">
+      <div class="hero__text">
         </a>
       </div>
       <div class="background"></div>

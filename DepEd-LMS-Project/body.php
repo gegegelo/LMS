3d5d65afaed218046f72198da9f338411bb3d265
Angelo A. Ramos <66189567+gegegelo@users.noreply.github.com>
2023-02-23 08:29:55 +0800

    Update body.php

--- DepEd-LMS-Project/body.php
+++ DepEd-LMS-Project/body.php
@@ -15,32 +15,259 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-8">
-                    <h1>Main Content</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et quam sed lacus bibendum vestibulum. Pellentesque vel mi ut sem laoreet consequat id id sem. Donec ut bibendum sapien. Vestibulum eu purus vel est fermentum porttitor. Nunc consectetur mi id sapien accumsan, eu tincidunt dolor fermentum. Donec consequat orci quam, in pharetra nibh tristique vel. Etiam rhoncus justo enim, ut bibendum nisi efficitur et.</p>
-                    <img src="img/vp-sara.png" class="w-50">
+                <img src="img/vp-sara.png" class="w-50">
+                    <h2><u>Sara Z. Duterte</u></h2>
+                    <h6>DepEd Secretary/Philippine Vice President</h6>
+                    <br>
+                        <p>Vice President Sara Duterte, who sits as the Department of Education (DepEd) Secretary in a concurrent capacity, reminded officials and personnel of the agency to practice the “highest form of professionalism” in the implementation and delivery of basic education programs and services.</p>
+                        <p>“To realize this, the DepEd should be free from any partisan activities,” she explained.</p>
+                        <p>Given this, Duterte reminded the officials and employees — which include teaching and non-teaching personnel in schools — to practice the “highest form of professionalism.”</p>
+                        <p>DepEd officials and employees were also asked to observe several measures to ensure the promotion of professionalism in the implementation and delivery of basic education programs and services.</p> 
                 </div>
                 <div class="col-md-4">
-                    <h2>Sidebar Content</h2>
+                    <div class="deped_web">
+                    <h4>Other DepEd Websites:</h4>
+                    </div>
                     <ul>
+                        <a href = "https://sites.google.com/deped.gov.ph/depedetulayeduplaylist/home">
                         <img src="img/deped-tulay.png" class="w-50">
+                        </a>
+                        <a href = "https://commons.deped.gov.ph/">
                         <img src="img/deped-commons.png" class="w-50">
+                        </a>
+                        <a href = "https://lms.deped.gov.ph/category/deped-tv/">
                         <img src="img/deped-tv.png" class="w-50">
+                        </a>
+                        <a href = "https://lrmds.deped.gov.ph/">
                         <img src="img/deped-lrmds.png" class="w-50">
+                        </a>
                     </ul>
                     <p>In Partnership with:</p>
+                    <a href = "https://www.jcimakati.com/">
                     <img src="img/jci-1.png" class="w-50">
+                    </a>
                     <img src="img/jci-2.png" class="w-50">
                     <img src="img/jci-3.png" class="w-50">
                 </div>
             </div>
         </div>
-    </section>
-    <section class="vh-100 section-padding">
+    </section>     
+</body>
+</html><!DOCTYPE html>
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DepEd E-Learning Management</title>
+    <?php require 'links.php'; ?>
+
+    <?php include 'script.php'; ?>
+</head>
+<body>
+    <hr />
+      <!-- Main content goes here -->
+    <section class="section-padding">
         <div class="container">
             <div class="row">
-                <div class="col-12 text-center">
-                    <h1 class="fw-bold">Title</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et quam sed lacus bibendum vestibulum.</p>
+                <div class="col-md-8">
+                <img src="img/vp-sara.png" class="w-50">
+                    <h2><u>Sara Z. Duterte</u></h2>
+                    <h6>DepEd Secretary/Philippine Vice President</h6>
+                    <br>
+                        <p>Vice President Sara Duterte, who sits as the Department of Education (DepEd) Secretary in a concurrent capacity, reminded officials and personnel of the agency to practice the “highest form of professionalism” in the implementation and delivery of basic education programs and services.</p>
+                        <p>“To realize this, the DepEd should be free from any partisan activities,” she explained.</p>
+                        <p>Given this, Duterte reminded the officials and employees — which include teaching and non-teaching personnel in schools — to practice the “highest form of professionalism.”</p>
+                        <p>DepEd officials and employees were also asked to observe several measures to ensure the promotion of professionalism in the implementation and delivery of basic education programs and services.</p> 
+                </div>
+                <div class="col-md-4">
+                    <div class="deped_web">
+                    <h4>Other DepEd Websites:</h4>
+                    </div>
+                    <ul>
+                        <a href = "https://sites.google.com/deped.gov.ph/depedetulayeduplaylist/home">
+                        <img src="img/deped-tulay.png" class="w-50">
+                        </a>
+                        <a href = "https://commons.deped.gov.ph/">
+                        <img src="img/deped-commons.png" class="w-50">
+                        </a>
+                        <a href = "https://lms.deped.gov.ph/category/deped-tv/">
+                        <img src="img/deped-tv.png" class="w-50">
+                        </a>
+                        <a href = "https://lrmds.deped.gov.ph/">
+                        <img src="img/deped-lrmds.png" class="w-50">
+                        </a>
+                    </ul>
+                    <p>In Partnership with:</p>
+                    <a href = "https://www.jcimakati.com/">
+                    <img src="img/jci-1.png" class="w-50">
+                    </a>
+                    <img src="img/jci-2.png" class="w-50">
+                    <img src="img/jci-3.png" class="w-50">
+                </div>
+            </div>
+        </div>
+    </section>     
+</body>
+</html><!DOCTYPE html>
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DepEd E-Learning Management</title>
+    <?php require 'links.php'; ?>
+
+    <?php include 'script.php'; ?>
+</head>
+<body>
+    <hr />
+      <!-- Main content goes here -->
+    <section class="section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                <img src="img/vp-sara.png" class="w-50">
+                    <h2><u>Sara Z. Duterte</u></h2>
+                    <h6>DepEd Secretary/Philippine Vice President</h6>
+                    <br>
+                        <p>Vice President Sara Duterte, who sits as the Department of Education (DepEd) Secretary in a concurrent capacity, reminded officials and personnel of the agency to practice the “highest form of professionalism” in the implementation and delivery of basic education programs and services.</p>
+                        <p>“To realize this, the DepEd should be free from any partisan activities,” she explained.</p>
+                        <p>Given this, Duterte reminded the officials and employees — which include teaching and non-teaching personnel in schools — to practice the “highest form of professionalism.”</p>
+                        <p>DepEd officials and employees were also asked to observe several measures to ensure the promotion of professionalism in the implementation and delivery of basic education programs and services.</p> 
+                </div>
+                <div class="col-md-4">
+                    <div class="deped_web">
+                    <h4>Other DepEd Websites:</h4>
+                    </div>
+                    <ul>
+                        <a href = "https://sites.google.com/deped.gov.ph/depedetulayeduplaylist/home">
+                        <img src="img/deped-tulay.png" class="w-50">
+                        </a>
+                        <a href = "https://commons.deped.gov.ph/">
+                        <img src="img/deped-commons.png" class="w-50">
+                        </a>
+                        <a href = "https://lms.deped.gov.ph/category/deped-tv/">
+                        <img src="img/deped-tv.png" class="w-50">
+                        </a>
+                        <a href = "https://lrmds.deped.gov.ph/">
+                        <img src="img/deped-lrmds.png" class="w-50">
+                        </a>
+                    </ul>
+                    <p>In Partnership with:</p>
+                    <a href = "https://www.jcimakati.com/">
+                    <img src="img/jci-1.png" class="w-50">
+                    </a>
+                    <img src="img/jci-2.png" class="w-50">
+                    <img src="img/jci-3.png" class="w-50">
+                </div>
+            </div>
+        </div>
+    </section>     
+</body>
+</html><!DOCTYPE html>
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DepEd E-Learning Management</title>
+    <?php require 'links.php'; ?>
+
+    <?php include 'script.php'; ?>
+</head>
+<body>
+    <hr />
+      <!-- Main content goes here -->
+    <section class="section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                <img src="img/vp-sara.png" class="w-50">
+                    <h2><u>Sara Z. Duterte</u></h2>
+                    <h6>DepEd Secretary/Philippine Vice President</h6>
+                    <br>
+                        <p>Vice President Sara Duterte, who sits as the Department of Education (DepEd) Secretary in a concurrent capacity, reminded officials and personnel of the agency to practice the “highest form of professionalism” in the implementation and delivery of basic education programs and services.</p>
+                        <p>“To realize this, the DepEd should be free from any partisan activities,” she explained.</p>
+                        <p>Given this, Duterte reminded the officials and employees — which include teaching and non-teaching personnel in schools — to practice the “highest form of professionalism.”</p>
+                        <p>DepEd officials and employees were also asked to observe several measures to ensure the promotion of professionalism in the implementation and delivery of basic education programs and services.</p> 
+                </div>
+                <div class="col-md-4">
+                    <div class="deped_web">
+                    <h4>Other DepEd Websites:</h4>
+                    </div>
+                    <ul>
+                        <a href = "https://sites.google.com/deped.gov.ph/depedetulayeduplaylist/home">
+                        <img src="img/deped-tulay.png" class="w-50">
+                        </a>
+                        <a href = "https://commons.deped.gov.ph/">
+                        <img src="img/deped-commons.png" class="w-50">
+                        </a>
+                        <a href = "https://lms.deped.gov.ph/category/deped-tv/">
+                        <img src="img/deped-tv.png" class="w-50">
+                        </a>
+                        <a href = "https://lrmds.deped.gov.ph/">
+                        <img src="img/deped-lrmds.png" class="w-50">
+                        </a>
+                    </ul>
+                    <p>In Partnership with:</p>
+                    <a href = "https://www.jcimakati.com/">
+                    <img src="img/jci-1.png" class="w-50">
+                    </a>
+                    <img src="img/jci-2.png" class="w-50">
+                    <img src="img/jci-3.png" class="w-50">
+                </div>
+            </div>
+        </div>
+    </section>     
+</body>
+</html><!DOCTYPE html>
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DepEd E-Learning Management</title>
+    <?php require 'links.php'; ?>
+
+    <?php include 'script.php'; ?>
+</head>
+<body>
+    <hr />
+      <!-- Main content goes here -->
+    <section class="section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                <img src="img/vp-sara.png" class="w-50">
+                    <h2><u>Sara Z. Duterte</u></h2>
+                    <h6>DepEd Secretary/Philippine Vice President</h6>
+                    <br>
+                        <p>Vice President Sara Duterte, who sits as the Department of Education (DepEd) Secretary in a concurrent capacity, reminded officials and personnel of the agency to practice the “highest form of professionalism” in the implementation and delivery of basic education programs and services.</p>
+                        <p>“To realize this, the DepEd should be free from any partisan activities,” she explained.</p>
+                        <p>Given this, Duterte reminded the officials and employees — which include teaching and non-teaching personnel in schools — to practice the “highest form of professionalism.”</p>
+                        <p>DepEd officials and employees were also asked to observe several measures to ensure the promotion of professionalism in the implementation and delivery of basic education programs and services.</p> 
+                </div>
+                <div class="col-md-4">
+                    <div class="deped_web">
+                    <h4>Other DepEd Websites:</h4>
+                    </div>
+                    <ul>
+                        <a href = "https://sites.google.com/deped.gov.ph/depedetulayeduplaylist/home">
+                        <img src="img/deped-tulay.png" class="w-50">
+                        </a>
+                        <a href = "https://commons.deped.gov.ph/">
+                        <img src="img/deped-commons.png" class="w-50">
+                        </a>
+                        <a href = "https://lms.deped.gov.ph/category/deped-tv/">
+                        <img src="img/deped-tv.png" class="w-50">
+                        </a>
+                        <a href = "https://lrmds.deped.gov.ph/">
+                        <img src="img/deped-lrmds.png" class="w-50">
+                        </a>
+                    </ul>
+                    <p>In Partnership with:</p>
+                    <a href = "https://www.jcimakati.com/">
+                    <img src="img/jci-1.png" class="w-50">
+                    </a>
+                    <img src="img/jci-2.png" class="w-50">
+                    <img src="img/jci-3.png" class="w-50">
                 </div>
             </div>
         </div>
